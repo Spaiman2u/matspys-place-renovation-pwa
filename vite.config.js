1f2e3a85213df5441f9016aa0446fc00e3af447f
Spaiman2u <spyrosginos@gmail.com>
2025-05-15 22:45:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,24 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
       manifest: {
-        name: "Matspys Renovation Planner",
-        short_name: "Renovator",
-        start_url: ".",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#3f51b5",
-        icons: []
-      }
-    })
-  ]
+        name: 'Matspys Renovation Planner',
+        short_name: 'RenovationApp',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon',
+          },
+        ],
+      },
+    }),
+  ],
+  base: '/',
 });
+
